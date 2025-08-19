05233f368291a03787b7456633bb0c3093874689
HJ <729748040@qq.com>
2025-08-20 01:41:47 +0800

    穿山甲接入

--- Podfile
+++ Podfile
@@ -1,9 +1,19 @@
 platform :ios, '13.0'
+# 声明使用csjm插件
+plugin 'cocoapods-byte-csjm'
 use_frameworks!
+# 设置为自动更新adapter版本号
+use_gm_adapter_update!
 
+# 设置线上构建target不自动更新（按照自己需要进行调整，可选）
 target 'renminglegou' do
   # 网络库
   pod 'Alamofire', '~> 5.8'
+  pod 'Ads-CN-Beta', '7.1.0.1', :subspecs => ['CSJMediation']
+  # 引入使用到的ADN SDK，开发者请按需引入
+  
+  # 设置线上打包tag,该tag不再执行adapter自动更新，此时以adapter自动更新的最后一次结果去加载各adapter的版本
+#  gm_release_target!
   
   # 如果需要其他库，可以逐个取消注释测试
   # pod 'SDWebImageSwiftUI', '~> 2.2'
--- renminglegou/renminglegouApp.swift
+++ renminglegou/renminglegouApp.swift
@@ -6,12 +6,43 @@
 //
 
 import SwiftUI
+import BUAdSDK
 
 @main
 struct renminglegouApp: App {
+    
+    init() {
+        setupBUAdSDK()
+    }
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
+    }
+    
+    private func setupBUAdSDK() {
+        /******************** 初始化 ********************/
+        let configuration = BUAdSDKConfiguration()
+        // 使用聚合
+        configuration.useMediation = true
+        configuration.appID = "5706508"
+        // 隐私合规配置
+        // 是否限制个性化广告
+        configuration.mediation.limitPersonalAds = NSNumber(integerLiteral: 0)
+        // 是否限制程序化广告
+        configuration.mediation.limitProgrammaticAds = NSNumber(integerLiteral: 0)
+        // 是否禁止CAID
+//        configuration.mediation.forbiddenCAID = NSNumber(integerLiteral: 0)
+        // 主题模式
+        configuration.themeStatus = NSNumber(integerLiteral: 0)
+        
+        // 初始化
+        BUAdSDKManager.start(asyncCompletionHandler:{ success, error in
+            if success {
+                // 处理成功之后的逻辑
+            }
+        })
+
     }
 }
